8a64a59e8243d9cd6896bc738609d1ac9c7b311c
SemihTut <yalaantv@gmail.com>
2021-07-26 23:37:48 +0300

    retreiving data from excel functionality is done

--- src/main/java/pajutie/salary/Months.java
+++ src/main/java/pajutie/salary/Months.java
@@ -1,6 +1,6 @@
 package pajutie.salary;
 
 public enum Months {
-    January, February, March, April, May, June, July, August, September, October, November, December;
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
 
 }
--- src/main/java/pajutie/salary/Savings.java
+++ src/main/java/pajutie/salary/Savings.java
@@ -3,6 +3,6 @@ package pajutie.salary;
 public class Savings{
     public static void main(String[] args) {
         Income income = new Income();
-        System.out.println("moneyInMyPocket = " + income.moneyInMyPocket(Months.August));
+        System.out.println("moneyInMyPocket = " + String.format("%,.2f", income.moneyInMyPocket(Months.AUGUST)));
     }
 }
