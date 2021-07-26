4d6e1e6444db2768fe9f9484ce1325136ed8d143
SemihTut <yalaantv@gmail.com>
2021-07-26 20:46:14 +0300

    eco101

---
+++ src/main/java/pajutie/salary/Income.java
@@ -0,0 +1,22 @@
+package pajutie.salary;
+
+public class Income {
+    final private static int salary = 3500;
+
+    public double allExpenses(){
+        return ConfigReader.getAllExpensesAsMap().values()
+                .stream()
+                .filter(entry->!entry.contains("tax_rate"))
+                .mapToDouble(Double::valueOf)
+                .sum();
+
+    }
+
+    public double moneyInMyPocket(){
+        return salary-allExpenses()-taxes();
+    }
+
+    public double taxes(){
+        return salary*Double.parseDouble(ConfigReader.get("tax_rate"));
+    }
+}
---
+++ src/main/java/pajutie/salary/Savings.java
@@ -0,0 +1,8 @@
+package pajutie.salary;
+
+public class Savings {
+    public static void main(String[] args) {
+        Income income = new Income();
+        System.out.println("moneyInMyPocket = " + income.moneyInMyPocket());
+    }
+}
