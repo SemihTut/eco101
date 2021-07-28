ae3a6a56ca2c3fbbe5f88c4411cd84b2fab97253
SemihTut <yalaantv@gmail.com>
2021-07-28 10:15:05 +0300

    javac

--- src/main/java/pajutie/salary/Income.java
+++ src/main/java/pajutie/salary/Income.java
@@ -4,6 +4,10 @@ public class Income {
     private final static int salary = 3500;
     private final String path = System.getProperty("user.dir");
 
+    /**
+     *
+     * @return this method calculate the fixed expenses in the 'Savings.properties' files
+     */
     protected double fixedExpenses() {
         return ConfigReader.getAllExpensesAsMap().values()
                 .stream()
@@ -13,14 +17,28 @@ public class Income {
 
     }
 
+    /**
+     *
+     * @param months I should be used the Month.enum
+     * @return
+     */
     protected double moneyInMyPocket(Months months) {
         return salary - fixedExpenses() - taxes() - excelExpenses(months.toString());
     }
 
+    /**
+     *
+     * @return total tax expense
+     */
     protected double taxes() {
         return salary * Double.parseDouble(ConfigReader.get("tax_rate"));
     }
 
+    /**
+     *
+     * @param month each month should be in different sheet
+     * @return
+     */
     protected double excelExpenses(String month) {
         ExcelUtil excelUtil = new ExcelUtil(path + "/extraExpenses.xlsx", month);
         return excelUtil.getFirstDataRow()
